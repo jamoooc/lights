78a87110491d5198164ae429d42db66756389548
Jamie MacRae <jamoooc@gmail.com>
2022-12-16 12:04:41 +0000

    feat: add line init and on\off loop

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::time::Duration;
+use std::thread::sleep;
+use std::error::Error;
+use gpio_cdev::{Chip, LineRequestFlags};
+
+const L1: u32 = 13; // GPIO13 PIN 33
+const L2: u32 = 19; // GPIO19 PIN 35
+const L3: u32 = 26; // GPIO26 PIN 37
+const L4: u32 = 16; // GPIO16 PIN 36
+const L5: u32 = 20; // GPIO20 PIN 38
+const L6: u32 = 21; // GPIO21 PIN 40
+
+fn main() -> Result<(), Box<dyn Error>>{
+
+  // get the gpio adapater
+  let mut chip = match Chip::new("/dev/gpiochip0") {
+    Ok(c) => c,
+    Err(e) => panic!("Oh no! No chip found!{:#?}", e)
+  };
+  println!("chip: {:#?}", chip);
+
+  let light_pins = vec![ L1, L2, L3, L4, L5, L6 ];
+
+  let mut lights = vec![];
+
+  for (i, pin) in light_pins.iter().enumerate() {
+    let str = format!("light{}", i.to_string());
+    // Request a line to GPIO pins from the kernel. The default
+    // value is set to 0 so all lines are off on initialisation.
+    let light = chip
+      .get_line(*pin)?
+      .request(LineRequestFlags::OUTPUT, 0, &str)?;
+    lights.push(light);
+  }
+
+  loop {
+    for light in lights.iter() {
+      light.set_value(1)?;
+      sleep(Duration::from_millis(200));
+      light.set_value(0)?;
+      sleep(Duration::from_millis(200));
+    }
+  }
+}
